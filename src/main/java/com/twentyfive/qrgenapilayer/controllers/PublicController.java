df22369b7d9ca174d832ebefa5b3dd56a95bee46
twentyfive <twentyfivesrls@gmail.com>
2023-09-28 15:57:27 +0200

    public controller

---
+++ src/main/java/com/twentyfive/qrgenapilayer/controllers/PublicController.java
@@ -0,0 +1,15 @@
+package com.twentyfive.qrgenapilayer.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+@CrossOrigin(origins = "*")
+public class PublicController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
